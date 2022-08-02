e5ab8c14efeb715b169697b1e29510d596f6cc1c
Zaynab88 <106927091+Zaynab88@users.noreply.github.com>
2022-08-02 12:55:45 +0200

    Update Order.ts

--- src/routes/Order.ts
+++ src/routes/Order.ts
@@ -1,10 +1,34 @@
-
-
 import express from 'express'
 import { Order } from '../entities/Order';
 import { OrderLines } from '../entities/OrderLines';
 
 const router = express.Router();
+
+router.get("/:id", async (req, res) => {
+    try {
+        const id = +req.params.id;
+        const order = await Order.findOne({
+            where: { id }, // get the orders with the following relations confirmed --->
+            relations: {
+                orderLines: true,
+            }
+        });
+        if (!order)
+            return res.status(404).json({ message: "Order Not Found" });
+        return res.json(order);
+
+
+
+
+    } catch (error) {
+        return res.status(500).json({ message: error });
+
+
+    };
+
+})
+
+
 
 
 router.post('/', async (req, res) => {
@@ -48,20 +72,3 @@ if(!order) return res.status(404).send("order not found")
 
 
 export { router as ordersRouter }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
